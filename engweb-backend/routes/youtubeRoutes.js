96b0790e1f1b7ce7e2b9b811b71ec897f7fed752
sooieese <sujinjang609@gmail.com>
2024-07-12 21:07:22 +0900

    first commit

--- engweb-backend/routes/youtubeRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { fetchCaption } = require('../controllers/youtubeController');
-const router = express.Router();
-
-
-// 자막 텍스트 가져오기
-router.get('/captions/:videoId', fetchCaption);
-
-module.exports = router;
