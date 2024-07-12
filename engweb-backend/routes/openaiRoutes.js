4984e0a636b2bea80b5b86cce874046801de3c53
sooieese <sujinjang609@gmail.com>
2024-07-12 22:23:36 +0900

    push all

--- engweb-backend/routes/openaiRoutes.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const { fetchLearningExpressions } = require('../controllers/openaiController');
-const router = express.Router();
-
-// 학습 표현 가져오기
-router.post('/expressions/:videoId', fetchLearningExpressions);
